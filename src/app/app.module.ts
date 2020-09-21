2d189b3e910abda1b174688f42e83b2d51ca29f8
Ahmed Meskine <40129162+midoum@users.noreply.github.com>
2020-09-21 10:49:24 +0100

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatBadgeModule} from '@angular/material';
+import {MatBadgeModule} from '@angular/material'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RefrigirateurComponent } from './refrigirateur/refrigirateur.component';
@@ -24,6 +24,7 @@ import { chariotservice } from './chariotservice';
   
   ],
   imports: [
+    MatBadgeModule,
     BrowserModule,
     AppRoutingModule
   ],
