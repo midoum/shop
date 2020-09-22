2effc82308e9bec390ee3988b6128003ef3bd933
Ahmed Meskine <40129162+midoum@users.noreply.github.com>
2020-09-22 18:27:01 +0100

    added login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,25 +9,62 @@ import { Component, Input, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 @Input() sc:boolean=true;
 @Input() si:boolean=false;
-emailc=(document.getElementById('emailc')as HTMLInputElement).value;
-emaili=(document.getElementById('emaili')as HTMLInputElement).value;
-passc=(document.getElementById('passc')as HTMLInputElement).value;
-passi=(document.getElementById('passi')as HTMLInputElement).value;
-passci=(document.getElementById('passci')as HTMLInputElement).value;
-nom=(document.getElementById('nom')as HTMLInputElement).value;
-prenom=(document.getElementById('prenom')as HTMLInputElement).value;
-  constructor() { }
+
+
+  constructor( private router:Router) { }
 
   ngOnInit(): void {
   }
 showC(){
   this.sc=true;
   this.si=false;
+  var emailc=((document.getElementById("emailc") as HTMLInputElement).value);
+  var passc=((document.getElementById("passc") as HTMLInputElement).value);
+
+if(emailc==""){
+    alert("Tapez votre email")
+  }else if(passc==""){
+    alert("Tapez votre mot de passe")
+  }else{  
+    if(emailc.match("@")){
+    alert("Connexion avec succès")
+    this.router.navigate(['/acceuil']);
+  }else {
+    alert("l'email doit contenir @")
+  }
+  }
+  
 
 }
 showI(){
   this.si=true;
   this.sc=false
-  
+  var nom=((document.getElementById("nom") as HTMLInputElement).value);
+  var prenom=((document.getElementById("prenom") as HTMLInputElement).value);
+  var emaili=((document.getElementById("emaili") as HTMLInputElement).value);
+  var passi=((document.getElementById("passi") as HTMLInputElement).value);
+  var passci=((document.getElementById("passci") as HTMLInputElement).value);
+  if(nom==""){
+    alert("Tapez votre nom")
+  }else  if(prenom==""){
+    alert("Tapez votre prenom")
+  }else  if(emaili==""){
+    alert("Tapez votre email")
+  }else  if(passi==""){
+    alert("Tapez votre mot de passe")
+  } else if(passci==""){
+    alert("Tapez votre mot de passe à confirmé")
+   
+
+    }else  if(passi!==passci){
+      alert("Les mots de passe doivent être conforme")
+  }else {
+     if(emaili.match("@")){
+      alert("Connexion avec succès")
+    }else {
+      alert("l'email doit contenir @")
+    
+  }
+}
 }
 }
