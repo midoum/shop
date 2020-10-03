9acd3ee3be42cdc8a6a4cc89d80231b85ca70cb6
Ahmed Meskine <40129162+midoum@users.noreply.github.com>
2020-10-03 07:19:15 +0100

    new

--- src/app/chariot/chariot.component.ts
+++ src/app/chariot/chariot.component.ts
@@ -1,7 +1,10 @@
 import { Component, ComponentFactoryResolver, Input, OnInit } from '@angular/core';
 import { produit } from '../produit';
-
+import * as firebase from 'firebase';
 import { chariotservice } from '../chariotservice';
+import { AngularFirestore} from '@angular/fire/firestore';
+import { AuthService } from '../Authservice';
+
 
 
 
@@ -16,7 +19,19 @@ export class ChariotComponent implements OnInit {
 
   chariot:produit[]=[];
  
-  constructor(private c:chariotservice) { 
+  constructor(private c:chariotservice,private authservice:AuthService,) { 
+    const config = {
+      apiKey: "AIzaSyCes1086y_q4-mpWHXBg1e2wkprD6YWXX8",
+      authDomain: "shop-b2639.firebaseapp.com",
+      databaseURL: "https://shop-b2639.firebaseio.com",
+      projectId: "shop-b2639",
+      storageBucket: "shop-b2639.appspot.com",
+      messagingSenderId: "909077061093",
+      appId: "1:909077061093:web:1a5d5976be1ff6423fe30c",
+      measurementId: "G-CV7R0GWGX2"
+    };
+    firebase.initializeApp(config);
+ 
  this.chariot=c.getChariot();
  console.log(this.chariot)
  
@@ -53,5 +68,8 @@ export class ChariotComponent implements OnInit {
    
     
   }
+addtoCart(){
+
+}
 
 }
